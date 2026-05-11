68c93443d6c42aef695915209746d13c05f191dd
Novikov Aleksei <3900676@gmail.com>
2026-05-12 01:01:53 +0300

    fix

--- commerce/order/src/main/java/ru/yandex/practicum/order/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.yandex.practicum.order.model.OrderItemEntity;
-import ru.yandex.practicum.order.model.OrderItemId;
-
-public interface OrderItemRepository extends JpaRepository<OrderItemEntity, OrderItemId> {
-}
